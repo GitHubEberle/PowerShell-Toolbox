01021bf55344e9d2b303e53782e8d7d9a2903541
GitHubEberle <136599761+GitHubEberle@users.noreply.github.com>
2023-11-02 11:39:34 +0100

    Create setLocalAdminPassword.ps1
    add main script

---
+++ SetLocalAdminPassword/setLocalAdminPassword.ps1
@@ -0,0 +1,31 @@
+# Benutzername, Passwort und Beschreibung definieren
+$userToCheck = "edv-install"
+$newPassword = "XottoX789!"  # Ersetzen Sie dies durch Ihr gewünschtes Passwort
+$userDescription = "Lokaler Administrator"  # Ersetzen Sie dies durch Ihre gewünschte Beschreibung
+
+# Passwort in ein SecureString konvertieren
+$securePassword = ConvertTo-SecureString -AsPlainText $newPassword -Force
+
+try {
+    # Überprüfen, ob der Benutzer existiert
+    $user = Get-LocalUser -Name $userToCheck -ErrorAction Stop
+    Write-Output "Benutzer '$userToCheck' existiert bereits. Passwort wird aktualisiert."
+
+    # Aktualisieren des Passworts
+    $user | Set-LocalUser -Password $securePassword
+    Write-Output "Passwort für Benutzer '$userToCheck' wurde erfolgreich aktualisiert."
+
+} catch [Microsoft.PowerShell.Commands.UserNotFoundException] {
+    # Benutzer existiert nicht, also wird er erstellt
+    $newUser = New-LocalUser -Name $userToCheck -Password $securePassword -AccountNeverExpires -UserMayNotChangePassword -Description $userDescription
+    Write-Output "Benutzer '$userToCheck' wurde erstellt mit der Beschreibung: '$userDescription'."
+
+    # Benutzer zur Administratorengruppe hinzufügen
+    Add-LocalGroupMember -Group "Administratoren" -Member $userToCheck
+    Write-Output "Benutzer '$userToCheck' wurde zur Gruppe 'Administratoren' hinzugefügt."
+    exit 0  # Erfolg
+} catch {
+    # Allgemeiner Fehler
+    Write-Error "Ein Fehler ist aufgetreten: $_"
+    exit 1  # Fehler
+}
